daaaae21b8341f17fdb5f9669a502c697de00477
MR.ROBOT <kuzivladimir@rambler.ru>
2019-10-18 23:12:26 +0300

    Все переделал

--- grafiki.py
+++ grafiki.py
@@ -5,103 +5,139 @@ import matplotlib.pyplot as plt
 import numpy as np
 from curs2 import Gold
 
+# s(t) = A*Gнс(t)*Gдк(t)*cos(w*t+fi)
 
-def adapter(sequence): # Меняет 0 на -1
+#Fn = 1575.42  В МГц
+L = 1023
+T = 1 *pow(10,-3) # Длительность последовательности 
+Fdk = L/T # частота последовательности = 1.023 МГц
+A = 1
+# Так как частота Fn очень большая, мы перенесем наш сигнал на 5 МГц - промежуточная частота 
+Fn = 5 *pow(10,6) # Гц
+# Частота дискретезации равна 2*Fdk  = 2.046 МГц. Чтобы влезли еще боковые липестки возьмем fs = 16Мгц
+fs = 16*pow(10,6) # Гц частота дискретезация   
+N = T *fs-1 # колличество элементоа в T
+
+
+tt = np.arange(0, N/fs, 1/fs)
+
+sig =  np.sin(2*pi*tt*Fn)
+
+def CodePhaseVect(tt, Fdk):	# Ищем сколько элементов синуса умнажаются на элемент м-последовательности 
+	Code =[]
+	for i in tt:
+		c = int(i*Fdk) # Округление в меньшую сторону для получения индекса м-последовательности
+		#  Возможно надо умножить Fdk на 2
+		Code.append(c)
+	return Code
+
+
+def adapter(sequence): # Меняет 0 на 1, а 1 на -1
 	for i in range(len(sequence)):
 		if sequence[i]==0:
+			sequence[i]=1
+		else:
 			sequence[i]=-1
-		# else:
-		# 	sequence[i]=0
 	return sequence
 
-n = 1023
-f = 15.7542 
-fd = 1023
-t = np.arange(0, 99, 1/fd)
-j = pow(-1, 0.5)
-T = 0.001 # длительнось последовательности 1мс
 
+def ModulatedSig(sig, sequence, Code, modulsig = []):# Модулирует сигнал м-последовательностью
+	for i in range(len(sequence)):
+		for j in range(len(Code)):
+			if i == Code[j]:
+				modulsig.append(sig[j]*sequence[i])
+	return modulsig
 
-N = round((T/n)*2*f)
-
-
-
-x = np.sin(2*pi*t*f)
-
-
-plt.subplot(5,1,1)
-plt.plot(t,x)
-plt.axis([0,0.4, -1,1])
+def Sequence(sequence, Code, sequence2 = []): # дублирует значения м-последовательности 
+	for i in range(len(sequence)):
+		for j in range(len(Code)):
+			if i == Code[j]:
+				sequence2.append(sequence[i])
+	return sequence2
 
 
 
 sequence = Gold()  # получаем нашу м-последовательность 
 sequence = adapter(sequence)  # меняем 0 на -1
-sequence2 = []
 
-sequence3 =[]
-
-
-
-
-
-
-for i in sequence:
-	for j in range(99):
-		sequence2.append(i)
+Code = CodePhaseVect(tt, Fdk)
+modulsig = ModulatedSig(sig, sequence, Code)
+sig_sequence  = Sequence(sequence, Code)
+# print(modulsig)
+# print(len(sig))
+# print(len(Code))
+# print(len(modulsig))
 
 
-plt.subplot(5,1,2)
-plt.plot(t,sequence2)
+plt.subplots(nrows = 4, ncols = 1, figsize= (12,8), dpi = 100)
+plt.subplots_adjust(hspace =0.7 , top = 0.95)
 
 
+# Чистый сигнал 
+plt.subplot(4,1,1)
+plt.plot(tt,sig)
+plt.title('Чистый сигнал ')
+plt.axis([0, 0.000005, -1, 1])
 
-y = [sequence2[i]*x[i] for i in range(len(sequence2)) ]
+# последовательность
+plt.subplot(4,1,2)
+plt.plot(tt,sig_sequence)
+plt.axis([0, 0.000005, -1.2, 1.2])
 
-
-
-
-plt.subplot(5,1,2)
-plt.plot(t,y)
-plt.axis([0, 0.4, -1.5, 1.5 ])
-
-
-
-# # вычисляем преобразование Фурье. Сигнал действительный, поэтому надо использовать rfft
-
-
-
-# fr = (fd/2)*np.linspace(0,3,n/2)
-
-X = fft(y)
-
-fr = np.linspace(0,512,len(x)/2)
-
-X_m = absolute(X[0:np.size(fr/2)])
+# модулированный сигнал 
+plt.subplot(4,1,2)
+plt.plot(tt,modulsig)
+plt.title('Модулированный сигнал  ')
+plt.axis([0, 0.000005, -1.2, 1.2])
 
 
-plt.subplot(5,1,3)
-plt.plot(fr, X_m/max(X_m))
-plt.axis([0, 30, 0, 1])
+# # # вычисляем преобразование Фурье. Сигнал действительный, поэтому надо использовать rfft
 
 
 
-X_md = 10*np.log10(X_m)
+# # fr = (fd/2)*np.linspace(0,3,n/2)
 
-l = np.ones(30)
-g = range(30)
+Fourier = fft(modulsig)
+
+fr = np.linspace(pow(10,4),fs,len(sig))
+
+Fourier_m = absolute(Fourier)
+
+Fourier_db = 10*np.log10(Fourier_m)
+
+# нормированный спектр
+plt.subplot(4,1,3)
+plt.plot(fr, Fourier_m/max(Fourier_m))
+plt.title('Нормированный спектр  ')
+plt.axis([3*pow(10,6), 7*pow(10,6), 0, 1])
 
 
 
-plt.subplot(5,1,4)
-plt.plot(g, l-0.13 , c = 'red')
-plt.ylabel('0.87')
-plt.axis([0, 30, 0, 1])
+# нормированный спектр в dB
+plt.subplot(4,1,4)
+plt.plot(fr, Fourier_db/max(Fourier_db))
+plt.title('Нормированный спектр в dB ')
+plt.axis([3*pow(10,6), 7*pow(10,6), 0, 1])
+
+# красная линия по уровню 13,6 дБ
+plt.subplot(4,1,4)
+plt.plot(range(3*pow(10,6), 7*pow(10,6)), np.ones(len(range(3*pow(10,6), 7*pow(10,6))))-0.136 , c='red')
+plt.axis([3*pow(10,6), 7*pow(10,6), 0, 1])
 
 
 
-
-plt.subplot(5,1,4)
-plt.plot(fr, X_md/max(X_md))
-plt.axis([0, 30, 0, 1])
 plt.show()
+
+# l = np.ones(30)
+# g = range(30)
+
+
+
+
+
+
+
+# plt.subplot(5,1,4)
+# plt.plot(fr, X_md/max(X_md))
+# plt.axis([0, 30, 0, 1])
+# plt.show()
