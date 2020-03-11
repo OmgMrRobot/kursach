fba90d4c85df1ad343c480e392b020d5e5ec1f46
Robot <kuzivladimir@rambler.ru>
2020-03-11 22:52:34 +0300

    parser

---
+++ Parser.py
@@ -0,0 +1,163 @@
+import pdb
+import numpy as np
+import itertools as it
+from math import pi as PI
+
+
+class Class_Data():
+	"""docstring for ClassName"""
+	def __init__(self, Id, health, eccentricity, corr_to_inclination, 
+		rate_of_right_ascention, square_root_of_semi_major_axis, right_ascention_parameter,
+		argument_of_perigee, mean_anomaly, Af0m,  Af1, Af0l, week_almanac,
+		time_of_week_almanac ):
+
+		self.Id = Id
+		self.health = health
+		self.eccentricity = eccentricity
+		self.corr_to_inclination = corr_to_inclination
+		self.rate_of_right_ascention = rate_of_right_ascention
+		self.square_root_of_semi_major_axis = square_root_of_semi_major_axis
+		self.right_ascention_parameter = right_ascention_parameter
+		self.argument_of_perigee = argument_of_perigee
+		self.mean_anomaly = mean_anomaly
+		self.Af0m = Af0m
+		self.Af1 = Af1
+		self.Af0l = Af0l
+		self.week_almanac = week_almanac
+		self.time_of_week_almanac = time_of_week_almanac
+
+
+
+
+
+
+		
+
+# Data = []
+# lst = [x for x in range(1500)]
+
+# g = 0
+# for i in range(32):
+# 	Data.append(type('New_Class_Data%d' % i, (), 
+# 	{'Id': lst[g],
+# 	 'health' : lst[g + 1],
+# 	 'eccentricity' : lst[g + 2],
+# 	 'corr_to_inclination' : lst[g + 3],
+# 	 'rate_of_right_ascention' : lst[g + 4],
+# 	 'square_root_of_semi_major_axis' : lst[g + 5],
+# 	 'right_ascention_parameter' : lst[g + 6],
+# 	 'argument_of_perigee' : lst[g + 7],
+# 	 'mean_anomaly' : lst[g + 8],
+# 	 'Af0m' : lst[g + 9],
+# 	 'Af1' : lst[g + 10],
+# 	 'Af0l' : lst[g + 11],
+# 	 'week_almanac' : lst[g + 12],
+# 	 'time_of_week_almanac' : lst[g + 13] } ))
+# 	g += 13
+
+
+# print(Data)
+
+#d = Data[0]()
+
+
+# print(Data[0]())
+# print(d.Id)
+
+# d = Data[6]()
+# print(d.Id)
+
+
+
+
+def ft_parser():
+	my_file = open("001.txt", "r") 
+	string = my_file.read()
+	my_file.close()
+
+	string2 = []
+	lst = [] # лист для значений
+	Data = [] # лист для экземпляров класса
+	index = 0
+
+	string = string.split("GPS: ")  # разделяем по GPS:
+
+	# print(string)
+
+	for i in string: # разделяем по \n
+		string2 = string2 + i.split('\n')
+
+	string = [] # очистили лист
+
+	for j in range(1, len(string2)):  # разделяем по =
+		if (len(string2[j])):
+			string = string + string2[j].lstrip().split('=')	
+
+	for r in range(1, len(string), 2): # оставляем только значение
+		lst.append(string[r]) 
+
+	for i in range(round(len(lst)/14)): # создаем экземпляры класса и запаминаем
+		data = Class_Data(lst[index], lst[index + 1], lst[index + 2],
+		 lst[index + 3], lst[index + 4], lst[index+ 5], lst[index + 6],
+		 lst[index +7], lst[index + 8], lst[index + 9], lst[index + 10],
+		 lst[index + 11], lst[index + 12], lst[index + 13])
+
+		Data.append(data)
+		index += 14
+
+	# for i in Data:
+	# 	print(i.Id)
+
+	return (Data)
+
+
+def separate_bits(word):
+
+	word = list(it.chain(word))
+	word = [int(i) for i in word]
+	return word
+
+
+def swich_to_bits():
+
+	Data = ft_parser()
+
+	for i  in Data:
+		i.eccentricity = int( (float(i.eccentricity) / PI) / (2**(-21)))
+		i.corr_to_inclination =  int ((float(i.corr_to_inclination) / (2*PI)) / (2**(-19)))
+		i.rate_of_right_ascention = int ((float(i.rate_of_right_ascention) / PI) / (2**(-38)) * 1000) #???
+		i.square_root_of_semi_major_axis = int (float(i.square_root_of_semi_major_axis) / (2**(-11)))
+		i.right_ascention_parameter = int((float(i.right_ascention_parameter) / PI) / (2**(-23)))
+		i.argument_of_perigee = int((float(i.argument_of_perigee) / PI) / (2**(-23)))
+		i.mean_anomaly = int((float(i.mean_anomaly) / PI) / (2**(-23)))
+		i.Af0m = int(float(i.Af0m) / (2**(-20)) / 1000) # в секундах
+		i.Af1 = int(float(i.Af1) / (2**(-38))) # в секундах
+		# i.time_of_week_almanac = separate_bits(np.binary_repr(float(time_of_week_almanac), ))
+
+	for i in Data:
+		i.Id = separate_bits(np.binary_repr(int(i.Id), 6))
+		i.health = separate_bits(np.binary_repr(int(i.health), 8))
+		i.eccentricity = separate_bits(np.binary_repr(i.eccentricity, 16))
+		i.corr_to_inclination = separate_bits(np.binary_repr(i.corr_to_inclination, 16))
+		i.rate_of_right_ascention = separate_bits(np.binary_repr(i.rate_of_right_ascention, 16))
+		i.square_root_of_semi_major_axis = separate_bits(np.binary_repr(i.square_root_of_semi_major_axis, 24))
+		i.right_ascention_parameter = separate_bits(np.binary_repr(i.right_ascention_parameter, 24))
+		i.argument_of_perigee = separate_bits(np.binary_repr(i.argument_of_perigee, 24))
+		i.mean_anomaly = separate_bits(np.binary_repr(i.mean_anomaly, 24))
+		i.Af0m = separate_bits(np.binary_repr(i.Af0m, 11))
+		i.Af1 = separate_bits(np.binary_repr(i.Af1, 11))
+	# 	# i.week_almanac = separate_bits(np.binary_repr(float(week_almanac), ))
+	# 	# i.time_of_week_almanac = separate_bits(np.binary_repr(float(time_of_week_almanac), ))
+
+
+	# a = 0
+	# for i in Data:
+	# 	print(i.rate_of_right_ascention)
+	# 	a+=1
+	# print(a)
+	return (Data)
+
+
+if __name__ == "__main__":
+	swich_to_bits()
+
--- message.py
+++ message.py
@@ -2,6 +2,7 @@
 import pdb
 import numpy as np
 import itertools as it
+from Parser import swich_to_bits
 
 Message = [] # наше сообщение
 
@@ -31,6 +32,20 @@ def ft_Last_Bits(word, D29, D30):
 			sum_mod2^=D30
 		last_bits.append(sum_mod2)
 	return last_bits
+
+def free_word(): # Пустое слово
+	global Message
+
+	reserved = [0] * 24
+	D29 = Message[-2]
+	D30 = Message[-1]
+	word =  reserved
+	last_bits = []
+
+	last_bits = ft_Last_Bits(word, D29, D30)
+	word =  [j^D30 for j in word]
+	Message = Message + word + last_bits
+
 
 
 def TLM_WORD():
@@ -285,11 +300,611 @@ def Sub_frame_1():
 	Word_9()
 	Word_10()
 
-# def Sub_frame_2():
+def Sub_frame_2():
 
-# 	TLM_WORD()
-# 	HOW_WORD(sub_frame_id = 2)
+	TLM_WORD()
+	HOW_WORD(sub_frame_id = 2)
+
+	def Word_3():
+		global Message
+
+		IODE = 230
+		Crc = 12345
+		D29 = Message[-2]
+		D30 = Message[-1]
+
+		IODE = np.binary_repr(IODE, 8)
+		Crc = np.binary_repr(Crc, 16)
+
+
+		word = IODE + Crc
+		word = list(it.chain(word)) # разаединяю по элемента
+		word = [int(i) for i in word]# преобразую строки в инт
+		last_bits = ft_Last_Bits(word, D29, D30)
+		word =  [j^D30 for j in word]
+		Message = Message + word + last_bits
+
+	def Word_4():
+		global Message
+
+		delta_n = 12345
+		M0 = 1
+		D29 = Message[-2]
+		D30 = Message[-1]
+
+		delta_n = np.binary_repr(delta_n, 16)
+		M0 = np.binary_repr(M0, 32)[:8]
+
+		word = delta_n + M0
+		word = list(it.chain(word)) # разаединяю по элемента
+		word = [int(i) for i in word]# преобразую строки в инт
+		last_bits = ft_Last_Bits(word, D29, D30)
+		word =  [j^D30 for j in word]
+		Message = Message + word + last_bits
+
+	def Word_5():
+		global Message
+
+		M0 = 1
+
+		M0 = np.binary_repr(M0, 32)[8:]
+		D29 = Message[-2]
+		D30 = Message[-1]
+
+		word = M0
+		word = list(it.chain(word)) # разаединяю по элемента
+		word = [int(i) for i in word]# преобразую строки в инт
+		last_bits = ft_Last_Bits(word, D29, D30)
+		word =  [j^D30 for j in word]
+		Message = Message + word + last_bits
+
+	def Word_6():
+		global Message
+		
+		D29 = Message[-2]
+		D30 = Message[-1]
+		Cuc = 12345
+		e = 12345
+
+		Cuc = np.binary_repr(Cuc, 16)
+		e = np.binary_repr(e, 32)[:8]
+
+		word = Cuc + e
+		word = list(it.chain(word)) # разаединяю по элемента
+		word = [int(i) for i in word]# преобразую строки в инт
+		last_bits = ft_Last_Bits(word, D29, D30)
+		word =  [j^D30 for j in word]
+		Message = Message + word + last_bits
+
+	def Word_7():
+		global Message
+		
+		D29 = Message[-2]
+		D30 = Message[-1]
+		e = 12345
+
+		e = np.binary_repr(e, 32)[8:]
+
+		word = e
+		word = list(it.chain(word)) # разаединяю по элемента
+		word = [int(i) for i in word]# преобразую строки в инт
+		last_bits = ft_Last_Bits(word, D29, D30)
+		word =  [j^D30 for j in word]
+		Message = Message + word + last_bits
+
+	def Word_8():
+		global Message
+		
+		D29 = Message[-2]
+		D30 = Message[-1]
+		Cus = 12345
+		A = 12345
+
+		Cus = np.binary_repr(Cus, 16)
+		A = np.binary_repr(A, 32)[:8]
+
+		word = Cus + A
+		word = list(it.chain(word)) # разаединяю по элемента
+		word = [int(i) for i in word]# преобразую строки в инт
+		last_bits = ft_Last_Bits(word, D29, D30)
+		word =  [j^D30 for j in word]
+		Message = Message + word + last_bits
+
+	def Word_9():
+		global Message
+		
+		D29 = Message[-2]
+		D30 = Message[-1]
+		A = 12345
+
+		A = np.binary_repr(A, 32)[8:]
+
+		word = A
+		word = list(it.chain(word)) # разаединяю по элемента
+		word = [int(i) for i in word]# преобразую строки в инт
+		last_bits = ft_Last_Bits(word, D29, D30)
+		word =  [j^D30 for j in word]
+		Message = Message + word + last_bits
+
+	def Word_10():
+		global Message
+		
+		D29 = Message[-2]
+		D30 = Message[-1]
+		Toe = 1245
+		Fit_Interval_Flag = 0
+		AODO = 0
+
+		Toe = np.binary_repr(Toe, 16)
+		AODO = np.binary_repr(AODO, 5)
+		Fit_Interval_Flag = np.binary_repr(Fit_Interval_Flag, 1)
+
+		word = Toe + Fit_Interval_Flag + AODO 
+		word = list(it.chain(word)) # разаединяю по элемента
+		word = [int(i) for i in word]# преобразую строки в инт
+
+		def D_29():
+			lst = indexs[29][:-1]
+			sum_mod2 = 0
+
+			for index in lst:
+				sum_mod2 ^= word[index - 1]
+			sum_mod2 ^= D30
+			if sum_mod2 == 1:
+				d24 =[1]
+			else:
+				d24 = [0] 
+			return  d24# находим значение d24
+
+		def D_30(word):
+			lst = indexs[30][:-1]
+			sum_mod2 = 0
+
+			for index in lst:
+				sum_mod2 ^= word[index - 1]
+			sum_mod2^=D29
+			if sum_mod2 == 1:
+				d23 =[1]
+			else:
+				d23 = [0] 
+
+			return  d23# находим значение d23
+
+		def Bits_23_24(word): # получаем слово с двумя упраляющими битами 
+			d24 = D_29()
+			d23 = D_30(word+d24)
+			return word+d23+d24
+
+
+		word = Bits_23_24(word)
+		last_bits = ft_Last_Bits(word, D29, D30)
+		word =  [j^D30 for j in word]
+		Message = Message + word + last_bits
+
+	Word_3()
+	Word_4()
+	Word_5()
+	Word_6()
+	Word_7()
+	Word_8()
+	Word_9()
+	Word_10()
+
+def Sub_frame_3():
+
+	TLM_WORD()
+	HOW_WORD(sub_frame_id = 3)
+
+	def Word_3():
+		global Message
+
+		Cic = 230
+		right_ascen_at_week  = 12345
+		D29 = Message[-2]
+		D30 = Message[-1]
+
+		right_ascen_at_week  = np.binary_repr(right_ascen_at_week, 32)[:8]
+		Cic = np.binary_repr(Cic, 16)
+
+
+		word = Cic + right_ascen_at_week
+		word = list(it.chain(word))
+		word = [int(i) for i in word]
+		last_bits = ft_Last_Bits(word, D29, D30)
+		word =  [j^D30 for j in word]
+		Message = Message + word + last_bits
+
+	def Word_4():
+		global Message
+
+		right_ascen_at_week  = 12345
+		D29 = Message[-2]
+		D30 = Message[-1]
+
+		right_ascen_at_week  = np.binary_repr(right_ascen_at_week, 32)[8:]
+
+		word = right_ascen_at_week
+		word = list(it.chain(word))
+		word = [int(i) for i in word]
+		last_bits = ft_Last_Bits(word, D29, D30)
+		word =  [j^D30 for j in word]
+		Message = Message + word + last_bits
+
+	def Word_5():
+		global Message
+
+		Cis = 230
+		i  = 12345
+		D29 = Message[-2]
+		D30 = Message[-1]
+
+		i  = np.binary_repr(i, 32)[:8]
+		Cis = np.binary_repr(Cis, 16)
+
+
+		word = Cis + i
+		word = list(it.chain(word))
+		word = [int(i) for i in word]
+		last_bits = ft_Last_Bits(word, D29, D30)
+		word =  [j^D30 for j in word]
+		Message = Message + word + last_bits
+
+	def Word_6():
+		global Message
+
+		I  = 12345
+		D29 = Message[-2]
+		D30 = Message[-1]
+
+		I  = np.binary_repr(I, 32)[8:]
+
+		word = I
+		word = list(it.chain(word))
+		word = [int(i) for i in word]
+		last_bits = ft_Last_Bits(word, D29, D30)
+		word =  [j^D30 for j in word]
+		Message = Message + word + last_bits
+
+	def Word_7():
+		global Message
+
+		Crc = 230
+		w  = 12345
+		D29 = Message[-2]
+		D30 = Message[-1]
+
+		w  = np.binary_repr(w, 32)[:8]
+		Crc = np.binary_repr(Crc, 16)
+
+
+		word = Crc + w
+		word = list(it.chain(word)) 
+		word = [int(i) for i in word]
+		last_bits = ft_Last_Bits(word, D29, D30)
+		word =  [j^D30 for j in word]
+		Message = Message + word + last_bits
+
+	def Word_8():
+		global Message
+
+		w  = 12345
+		D29 = Message[-2]
+		D30 = Message[-1]
+
+		w  = np.binary_repr(w, 32)[8:]
+
+		word = w
+		word = list(it.chain(word))
+		word = [int(i) for i in word]
+		last_bits = ft_Last_Bits(word, D29, D30)
+		word =  [j^D30 for j in word]
+		Message = Message + word + last_bits
+
+	def Word_9():
+		global Message
+
+		dqdt  = 12345
+		D29 = Message[-2]
+		D30 = Message[-1]
+
+		dqdt  = np.binary_repr(dqdt, 24)
+
+		word = dqdt
+		word = list(it.chain(word))
+		word = [int(i) for i in word]
+		last_bits = ft_Last_Bits(word, D29, D30)
+		word =  [j^D30 for j in word]
+		Message = Message + word + last_bits
+
+
+	def Word_10():
+		global Message
+
+		IODE  = 123
+		IDOT = 12345
+		D29 = Message[-2]
+		D30 = Message[-1]
+
+		IODE  = np.binary_repr(IODE, 8)
+		IDOT  = np.binary_repr(IDOT, 14)
+
+		word = IODE + IDOT
+		word = list(it.chain(word)) # разаединяю по элемента
+		word = [int(i) for i in word]# преобразую строки в инт
+
+		def D_29():
+			lst = indexs[29][:-1]
+			sum_mod2 = 0
+
+			for index in lst:
+				sum_mod2 ^= word[index - 1]
+			sum_mod2 ^= D30
+			if sum_mod2 == 1:
+				d24 =[1]
+			else:
+				d24 = [0] 
+			return  d24# находим значение d24
+
+		def D_30(word):
+			lst = indexs[30][:-1]
+			sum_mod2 = 0
+
+			for index in lst:
+				sum_mod2 ^= word[index - 1]
+			sum_mod2^=D29
+			if sum_mod2 == 1:
+				d23 =[1]
+			else:
+				d23 = [0] 
+
+			return  d23# находим значение d23
+
+		def Bits_23_24(word): # получаем слово с двумя упраляющими битами 
+			d24 = D_29()
+			d23 = D_30(word+d24)
+			return word+d23+d24
+
+
+		word = Bits_23_24(word)
+		last_bits = ft_Last_Bits(word, D29, D30)
+		word =  [j^D30 for j in word]
+		Message = Message + word + last_bits
+
+	Word_3()
+	Word_4()
+	Word_5()
+	Word_6()
+	Word_7()
+	Word_8()
+	Word_9()
+	Word_10()
+
+def Sub_frame_4():
+
+	TLM_WORD()
+	HOW_WORD(sub_frame_id = 4)
+
+	def Word_3_9():
+		for i in range(7):
+			free_word()
+
+	def Word_10():
+		global Message
+
+		D29 = Message[-2]
+		D30 = Message[-1]
+
+		word = [0] * 22
+
+
+		def D_29():
+			lst = indexs[29][:-1]
+			sum_mod2 = 0
+
+			for index in lst:
+				sum_mod2 ^= word[index - 1]
+			sum_mod2 ^= D30
+			if sum_mod2 == 1:
+				d24 =[1]
+			else:
+				d24 = [0] 
+			return  d24# находим значение d24
+
+		def D_30(word):
+			lst = indexs[30][:-1]
+			sum_mod2 = 0
+
+			for index in lst:
+				sum_mod2 ^= word[index - 1]
+			sum_mod2^=D29
+			if sum_mod2 == 1:
+				d23 =[1]
+			else:
+				d23 = [0] 
+
+			return  d23# находим значение d23
+
+		def Bits_23_24(word): # получаем слово с двумя упраляющими битами 
+			d24 = D_29()
+			d23 = D_30(word+d24)
+			return word+d23+d24
+
+		word = Bits_23_24(word)
+		last_bits = ft_Last_Bits(word, D29, D30)
+		word =  [j^D30 for j in word]
+		Message = Message + word + last_bits
+
+	Word_3_9()
+	Word_10()
+
+
+def Sub_frame_5(Data):
+	TLM_WORD()
+	HOW_WORD()
+
+
+	def Word_3(): #alarm
+		global Message
+		data_id = [0] * 2
+		sv_id = [0] * 6
+
+		word = data_id + sv_id + Data.eccentricity
+		D29 = Message[-2]
+		D30 = Message[-1]
+
+		last_bits = ft_Last_Bits(word, D29, D30)
+		word =  [j^D30 for j in word]
+		Message = Message + word + last_bits
+		
+
+	def Word_4():  #alarm
+		global Message
+		Toa = [0] * 8
+
+		word = Toa + Data.corr_to_inclination[:16] #!!!!!!
+		# print(Data.corr_to_inclination)
+		# print(len(Data.corr_to_inclination))
+		D29 = Message[-2]
+		D30 = Message[-1]
+
+		last_bits = ft_Last_Bits(word, D29, D30)
+		word =  [j^D30 for j in word]
+		Message = Message + word + last_bits
+
+	def Word_5():
+		global Message
+
+		word = Data.rate_of_right_ascention + Data.health
+		D29 = Message[-2]
+		D30 = Message[-1]
+
+		last_bits = ft_Last_Bits(word, D29, D30)
+		word =  [j^D30 for j in word]
+		Message = Message + word + last_bits
+
+	def Word_6():
+		global Message
+
+		word = Data.square_root_of_semi_major_axis
+		D29 = Message[-2]
+		D30 = Message[-1]
+
+		last_bits = ft_Last_Bits(word, D29, D30)
+		word =  [j^D30 for j in word]
+		Message = Message + word + last_bits
+
+	def Word_7():
+		global Message
+
+		word = Data.right_ascention_parameter
+		D29 = Message[-2]
+		D30 = Message[-1]
+
+		last_bits = ft_Last_Bits(word, D29, D30)
+		word =  [j^D30 for j in word]
+		Message = Message + word + last_bits
+
+	def Word_8():
+		global Message
+
+		word = Data.argument_of_perigee
+		D29 = Message[-2]
+		D30 = Message[-1]
+
+		last_bits = ft_Last_Bits(word, D29, D30)
+		word =  [j^D30 for j in word]
+		Message = Message + word + last_bits
+
+	def Word_9():
+		global Message
+
+		word = Data.mean_anomaly
+		D29 = Message[-2]
+		D30 = Message[-1]
+
+		last_bits = ft_Last_Bits(word, D29, D30)
+		word =  [j^D30 for j in word]
+		Message = Message + word + last_bits
+
+	def Word_10():
+		global Message
+
+		word = Data.Af0m[:8] + Data.Af1 + Data.Af0m[8:]
+		D29 = Message[-2]
+		D30 = Message[-1]
+
+		def D_29():
+			lst = indexs[29][:-1]
+			sum_mod2 = 0
+
+			for index in lst:
+				sum_mod2 ^= word[index - 1]
+			sum_mod2 ^= D30
+			if sum_mod2 == 1:
+				d24 =[1]
+			else:
+				d24 = [0] 
+			return  d24# находим значение d24
+
+		def D_30(word):
+			lst = indexs[30][:-1]
+			sum_mod2 = 0
+
+			for index in lst:
+				sum_mod2 ^= word[index - 1]
+			sum_mod2^=D29
+			if sum_mod2 == 1:
+				d23 =[1]
+			else:
+				d23 = [0] 
+
+			return  d23# находим значение d23
+
+		def Bits_23_24(word): # получаем слово с двумя упраляющими битами 
+			d24 = D_29()
+			d23 = D_30(word+d24)
+			return word+d23+d24
+
+		word = Bits_23_24(word)
+		last_bits = ft_Last_Bits(word, D29, D30)
+		word =  [j^D30 for j in word]
+		Message = Message + word + last_bits
+
+
+
+
+	Word_3()
+	Word_4() # kos
+	Word_5()
+	Word_6()
+	Word_7()
+	Word_8()
+	Word_9()
+	Word_10()
+
+
+
+
+
 	
-Sub_frame_1()
+
+
+def Main_Message():
+
+	Data = swich_to_bits()
+	Sub_frame_1()
+	Sub_frame_2()
+	Sub_frame_3()
+
+	for i in range(24):
+		Sub_frame_4()
+		Sub_frame_5(Data[i])
+	# страница 25 с состоянием спутников
+	Sub_frame_4()
+	Sub_frame_4()
+
+
+
+Main_Message()
 print(Message)
 print(len(Message))
